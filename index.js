7d0906c5837625da93a85d048adee0e0d9f83eec
Tommy Lau <tlau425@yahoo.com>
2019-02-05 15:48:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var newArr = [];
+  for (var i = 0; i < musicians.length; i++) {
+    newArr.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return newArr;
+}
+
+function johnLennonFacts(arr) {
+  var outcome = [];
+  var i = 0;
+  while (i < arr.length) {
+    outcome.push(arr[i] + '!!!')
+    i++;
+  }
+  return outcome;
+}
+
+function iLoveTheBeatles(n) {
+  var newArr = [];
+  do {
+    newArr.push('I love the Beatles!');
+    n++;
+  }
+    while(n < 15);
+    return newArr;
+}
+
+  
